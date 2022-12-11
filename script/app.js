bbfcf243d52f0dcaee6e0441f528a94f251f69f7
Ouistitive <91791407+Ouistitive@users.noreply.github.com>
2022-12-11 17:19:05 +0100

    animation in navbar

---
+++ script/app.js
@@ -0,0 +1,7 @@
+var navElement = null;
+
+function init() {
+    var nav = document.getElementsByTagName("nav");
+    navElement = document.getElementsByTagName("ul").getElementsByTagName("li");
+    navElement[0].style.color = "orange";
+}
