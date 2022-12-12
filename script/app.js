423a7d6bf6b4737411b4e156569c38d87f77fa6c
Ouistitive <91791407+Ouistitive@users.noreply.github.com>
2022-12-12 15:23:05 +0100

    change color

--- script/app.js
+++ script/app.js
@@ -4,14 +4,32 @@ var i = 0;
 function init() {
     var nav = document.getElementById("nav-elements");
     navElement = nav.getElementsByTagName("a");
-    navElement[0].style.color = "orange";
+    navElement[0].style.color = "rgb(29, 210, 210)";
 
     for(var i = 1; i < navElement.length; i++)
         navElement[i].style.color = "white";
+
+
+
+    $('#tools').viewportChecker({
+        // Class to add to the elements when they are visible
+        classToAdd: 'visible',
+        
+        // The offset of the elements (let them appear earlier or later)
+        offset: 100,
+        
+        // Add the possibility to remove the class if the elements are not visible
+        repeat: false,
+        
+        // Callback to do after a class was added to an element. Action will return "add" or "remove", depending if the class was added or removed
+        callbackFunction: function(elem, action) {
+            alert("test");
+        }
+        });
 }
 
 function changeNavColor(idx) {
-    navElement[idx].style.color = "orange";
+    navElement[idx].style.color = "rgb(29, 210, 210)";
 
     for(var i = 1; i < navElement.length; i++)
         navElement[(idx + i) % navElement.length].style.color = "white";
