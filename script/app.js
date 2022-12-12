804d413b4c66a2ed622aec5a097536c91d525509
Ouistitive <91791407+Ouistitive@users.noreply.github.com>
2022-12-12 14:22:30 +0100

    color on nav bar

--- script/app.js
+++ script/app.js
@@ -1,7 +1,18 @@
 var navElement = null;
+var i = 0;
 
 function init() {
-    var nav = document.getElementsByTagName("nav");
-    navElement = document.getElementsByTagName("ul").getElementsByTagName("li");
+    var nav = document.getElementById("nav-elements");
+    navElement = nav.getElementsByTagName("a");
     navElement[0].style.color = "orange";
+
+    for(var i = 1; i < navElement.length; i++)
+        navElement[i].style.color = "white";
+}
+
+function changeNavColor(idx) {
+    navElement[idx].style.color = "orange";
+
+    for(var i = 1; i < navElement.length; i++)
+        navElement[(idx + i) % navElement.length].style.color = "white";
 }
